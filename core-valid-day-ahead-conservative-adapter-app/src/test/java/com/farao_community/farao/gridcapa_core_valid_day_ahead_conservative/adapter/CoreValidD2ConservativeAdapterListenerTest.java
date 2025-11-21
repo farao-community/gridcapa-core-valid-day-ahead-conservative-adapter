772e9ecbecc73415caf55f2a9e0c725a05b72839
Antoine LIMOUZIN <antoine.limouzin@rte-france.com>
2025-11-21 11:40:13 +0100

    change packages

--- core-valid-day-ahead-conservative-adapter-app/src/test/java/com/farao_community/farao/gridcapa_core_valid_day_ahead_conservative/adapter/CoreValidD2ConservativeAdapterListenerTest.java
+++ core-valid-day-ahead-conservative-adapter-app/src/test/java/com/farao_community/farao/gridcapa_core_valid_day_ahead_conservative/adapter/CoreValidD2ConservativeAdapterListenerTest.java
@@ -4,7 +4,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
-package com.farao_community.farao.gridcapa_core_valid_day_ahead_conservative;
+package com.farao_community.farao.gridcapa_core_valid_day_ahead_conservative.adapter;
 
 import com.farao_community.farao.gridcapa.task_manager.api.ProcessEventDto;
 import com.farao_community.farao.gridcapa.task_manager.api.ProcessFileDto;
